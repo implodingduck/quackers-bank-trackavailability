8a043a48cfd30350a438acea7be993368b9d1fb4
implodingduck <zsek320@gmail.com>
2021-10-04 18:44:03 -0500

    adding in acr and kv... working on a docker function

--- main.tf
+++ main.tf
@@ -62,3 +62,71 @@ module "func" {
       }
   ]
 }
+
+resource "azurerm_container_registry" "test" {
+  name                = "acr${local.func_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-${local.func_name}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
+
+  
+}
+
+resource "azurerm_key_vault_access_policy" "sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+  
+  key_permissions = [
+    "create",
+    "get",
+    "purge",
+    "recover",
+    "delete"
+  ]
+
+  secret_permissions = [
+    "set",
+    "purge",
+    "get",
+    "list"
+  ]
+
+  certificate_permissions = [
+    "purge"
+  ]
+
+  storage_permissions = [
+    "purge"
+  ]
+  
+}
+
+
+resource "azurerm_key_vault_access_policy" "as" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = module.func.identity_principal_id
+  
+  key_permissions = [
+    "get",
+  ]
+
+  secret_permissions = [
+    "get",
+    "list"
+  ]
+  
+}
--- qbtrackavailability/CheckAvailability.cs
+++ qbtrackavailability/CheckAvailability.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http; 
-
+using System.Collections.Generic;
+using System.Linq;
 using System.Diagnostics; 
 using Microsoft.ApplicationInsights; 
 using Microsoft.ApplicationInsights.Channel; 
@@ -12,7 +13,9 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace qbtrackavailability
 {
@@ -61,8 +64,15 @@ namespace qbtrackavailability
                 { 
                     activity.Start(); 
                     availability.Id = Activity.Current.SpanId.ToString(); 
-                    // Run business logic 
-                    await RunAvailabilityTestAsync(log); 
+                    var chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("--headless");
+                    chromeOptions.AddArgument("--disable-gpu");
+                    chromeOptions.AddArgument("--no-sandbox");
+                    using (var driver = new ChromeDriver(chromeOptions))
+                    {
+                        // Run business logic 
+                        await RunAvailabilityTestAsync(log, driver); 
+                    }
                 } 
                 availability.Success = true; 
             } 
@@ -84,14 +94,70 @@ namespace qbtrackavailability
             } 
         }
 
-        public async static Task RunAvailabilityTestAsync(ILogger log) 
+        public async static Task RunAvailabilityTestAsync(ILogger log, ChromeDriver driver) 
         { 
             log.LogInformation($"Attempting to run Availability Test");
-            using (var httpClient = new HttpClient()) 
-            { 
-                // TODO: Replace with your business logic 
-                await httpClient.GetStringAsync("https://frontendov5kihos.azurewebsites.net/health"); 
-            } 
+//             using (var httpClient = new HttpClient()) 
+//             { 
+//                 // TODO: Replace with your business logic 
+//                 await httpClient.GetStringAsync(""); 
+//             } 
+            
+            log.LogInformation("Pre get EnvironmentVariable");
+            //Navigate to DotNet website
+            var baseurl = Environment.GetEnvironmentVariable("BASE_URL");
+            log.LogInformation($"Going to {baseurl}");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+
+            //Navigate to base website
+            driver.Navigate().GoToUrl(baseurl);
+            IReadOnlyList<IWebElement> boxEles = driver.FindElements(By.CssSelector(".box"));
+            log.LogInformation(boxEles.ElementAt(0).Text);
+
+            //Click on the navbar toggle
+            IWebElement navbarToggler = driver.FindElement(By.CssSelector(".navbar-toggler.collapsed"));
+            navbarToggler.Click();
+
+            //Click on the login link
+            var loginEleXpath = By.XPath("//a[contains(text(),'Login')]");
+            wait.Until(ExpectedConditions.ElementIsVisible(loginEleXpath));
+            IWebElement loginEle = driver.FindElement(loginEleXpath);
+            log.LogInformation("Login button");
+            log.LogInformation($"Login Displayed: {loginEle.Displayed}");
+            log.LogInformation($"Login Enabled: {loginEle.Enabled}");
+            loginEle.Click();
+
+            //Wait for the B2C login page
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".intro")));
+            IReadOnlyList<IWebElement> introEles = driver.FindElements(By.CssSelector(".intro"));
+            log.LogInformation(introEles.ElementAt(0).Text);
+            
+            //fill out the form
+            var testemail = Environment.GetEnvironmentVariable("TEST_EMAIL");
+            var testpassword = Environment.GetEnvironmentVariable("TEST_PASSWORD");
+            driver.FindElement(By.Id("email")).SendKeys(testemail);
+            IWebElement passwordEle = driver.FindElement(By.Id("password"));
+            passwordEle.SendKeys(testpassword);
+            passwordEle.SendKeys(Keys.Enter);
+            
+            //Wait for login to complete
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".navbar-toggler.collapsed")));
+            log.LogInformation("Login has completed");
+            navbarToggler = driver.FindElement(By.CssSelector(".navbar-toggler.collapsed"));
+            navbarToggler.Click();
+
+            //Go to accounts
+            var accountEleXpath = By.XPath("//a[contains(text(),'Accounts')]");
+            wait.Until(ExpectedConditions.ElementIsVisible(accountEleXpath));
+            IWebElement accountEle = driver.FindElement(accountEleXpath);
+            accountEle.Click();
+            log.LogInformation("Clicking on Accounts");
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h3[contains(text(),'Checking')]")));
+            log.LogInformation("Accounts are visible");
+
+
+            
         } 
     }
 }
