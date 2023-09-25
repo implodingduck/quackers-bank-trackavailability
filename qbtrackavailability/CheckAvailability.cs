b95a15dcb184036f57085a7da4ac45a7ce6eda9a
implodingduck <zsek320@gmail.com>
2023-09-25 16:04:38 -0500

    updating selenium

--- qbtrackavailability/CheckAvailability.cs
+++ qbtrackavailability/CheckAvailability.cs
@@ -17,6 +17,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
+using SeleniumExtras.WaitHelpers;
+
 namespace qbtrackavailability
 {
     public static class CheckAvailability
