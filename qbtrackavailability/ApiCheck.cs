d65296324f9d19b608ad4156de97de0a6408d0f7
implodingduck <zsek320@gmail.com>
2023-09-26 11:09:52 -0500

    Adding API check

---
+++ qbtrackavailability/ApiCheck.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Net.Http; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics; 
+using Microsoft.ApplicationInsights; 
+using Microsoft.ApplicationInsights.Channel; 
+using Microsoft.ApplicationInsights.DataContracts; 
+using Microsoft.ApplicationInsights.Extensibility; 
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace qbtrackavailability
+{
+    public static class ApiCheck 
+    {
+        private static TelemetryClient telemetryClient; 
+        [FunctionName("ApiCheck")]
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log,  ExecutionContext executionContext)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            if (telemetryClient == null) 
+            { 
+                // Initializing a telemetry configuration for Application Insights based on connection string 
+
+                var telemetryConfiguration = new TelemetryConfiguration(); 
+                telemetryConfiguration.ConnectionString = Environment.GetEnvironmentVariable("AVAILABILITY_APPINSIGHTS_CONNECTION_STRING"); 
+                telemetryConfiguration.TelemetryChannel = new InMemoryChannel(); 
+                telemetryClient = new TelemetryClient(telemetryConfiguration); 
+            } 
+            log.LogInformation($"Telemetry Client has been initialized");
+            string testName = executionContext.FunctionName; 
+            log.LogInformation($"testName={testName}");
+            string location = Environment.GetEnvironmentVariable("REGION_NAME");
+            log.LogInformation($"location={location}");
+            var availability = new AvailabilityTelemetry 
+            { 
+                Name = testName, 
+
+                RunLocation = location, 
+
+                Success = false, 
+            }; 
+            log.LogInformation($"AvailabilityTelemetry has been set");
+
+            availability.Context.Operation.ParentId = Activity.Current.SpanId.ToString();
+            log.LogInformation($"ParentId={availability.Context.Operation.ParentId}");
+            availability.Context.Operation.Id = Activity.Current.RootId; 
+            log.LogInformation($"OperationId={availability.Context.Operation.Id}");
+            var stopwatch = new Stopwatch(); 
+            stopwatch.Start(); 
+            log.LogInformation($"StopWatch has started");
+            try 
+            { 
+                var activity = new Activity("AvailabilityContext");
+                if (activity != null)
+                { 
+                    activity.Start(); 
+                    availability.Id = Activity.Current.SpanId.ToString(); 
+                    await RunAvailabilityTestAsync(log);
+                }
+                availability.Success = true; 
+            } 
+
+            catch (Exception ex) 
+            { 
+                availability.Message = ex.Message; 
+                throw; 
+            } 
+
+            finally 
+            { 
+                stopwatch.Stop(); 
+                log.LogInformation($"StopWatch has stopped");
+                availability.Duration = stopwatch.Elapsed; 
+                availability.Timestamp = DateTimeOffset.UtcNow; 
+                telemetryClient.TrackAvailability(availability); 
+                telemetryClient.Flush(); 
+            } 
+        }
+        public async static Task RunAvailabilityTestAsync(ILogger log) 
+        { 
+            log.LogInformation($"Attempting to run Availability Test");
+            var baseurl = Environment.GetEnvironmentVariable("BASE_URL");
+            private static HttpClient httpClient = new()
+            {
+                BaseAddress = new Uri(baseurl),
+            };
+            using HttpResponseMessage response = await httpClient.GetAsync("health");
+            response.EnsureSuccessStatusCode()
+                .WriteRequestToConsole();
+    
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            log.LogInformation($"JSON Response: {jsonResponse}");
+            JsonNode responseNode = JsonNode.Parse(jsonResponse)!;
+            int responseId = (int)responseNode!["id"]!;
+            if responseId != 200{
+                throw new Exception("API response does not match value")
+            }
+            
+        }
+    }
+}
